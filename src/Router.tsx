8d6ffa63ca45841aac84b6a28400d11f79e8bdc0
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-24 14:57:33 -0400

    Agregado layout para compartir un header en las rutas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import FavoritesPage from "./pages/FavoritesPage";
+import Layout from "./layouts/layout";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
