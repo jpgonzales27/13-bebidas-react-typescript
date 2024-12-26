f80bd848323ebd27f8f1c54a072d96ba3af24aab
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-26 16:13:27 -0400

    usando devtools con zustand

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,6 +1,9 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 
-export const useAppStore = create<RecipesSliceType>((...a) => ({
-  ...createRecipesSlice(...a),
-}));
+export const useAppStore = create<RecipesSliceType>()(
+  devtools((...a) => ({
+    ...createRecipesSlice(...a),
+  }))
+);
