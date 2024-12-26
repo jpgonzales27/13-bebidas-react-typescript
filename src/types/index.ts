127298fc2734d74bab67dcdd3a298cc8c9d6af4b
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-26 17:17:05 -0400

    mostrando los resultados de los drinks y creado su card

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
-import { CategoriesAPIResponseSchema, DrinksAPIResponse, SearchFilterSchema } from "../schemas/recipes-schema";
+import {
+  CategoriesAPIResponseSchema,
+  DrinkAPIResponse,
+  DrinksAPIResponse,
+  SearchFilterSchema,
+} from "../schemas/recipes-schema";
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchema>;
 export type Drinks = z.infer<typeof DrinksAPIResponse>;
+export type Drink = z.infer<typeof DrinkAPIResponse>;
