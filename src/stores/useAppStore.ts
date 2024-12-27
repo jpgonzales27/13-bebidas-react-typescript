4b122b0bbd80e9f7c84299a3ecf164f7893c75c6
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-27 16:36:29 -0400

    creado slice para favoritos

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
+import { createFavoritesSlice, FavoritesSliceType } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
