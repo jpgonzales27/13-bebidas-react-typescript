f62b8426b12621725973c7ca0bbe0c8b82363187
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-27 16:54:45 -0400

    add y delete favorite cambiando mesaje en el boton

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -4,12 +4,13 @@ import { Recipe } from "../types";
 export type FavoritesSliceType = {
   favorites: Recipe[];
   handleClickFavorite: (recipe: Recipe) => void;
+  favoriteExists: (id: Recipe["idDrink"]) => boolean;
 };
 
 export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
   favorites: [],
   handleClickFavorite: (recipe) => {
-    if (get().favorites.find((fav) => fav.idDrink === recipe.idDrink)) {
+    if (get().favoriteExists(recipe.idDrink)) {
       set((state) => ({
         favorites: state.favorites.filter((fav) => fav.idDrink !== recipe.idDrink),
       }));
@@ -19,5 +20,8 @@ export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get)
         // favorites: [ ...get().favorites, recipe]
       }));
     }
+  },
+  favoriteExists(id) {
+    return get().favorites.some((favorite) => favorite.idDrink === id);
   },
 });
