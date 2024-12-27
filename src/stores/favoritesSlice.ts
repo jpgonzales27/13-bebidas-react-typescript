c698765a67ecfbe414e43e901fe159583cf7bc66
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-27 16:46:56 -0400

    agregado logica para añadir a favoritos

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -3,8 +3,21 @@ import { Recipe } from "../types";
 
 export type FavoritesSliceType = {
   favorites: Recipe[];
+  handleClickFavorite: (recipe: Recipe) => void;
 };
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
   favorites: [],
+  handleClickFavorite: (recipe) => {
+    if (get().favorites.find((fav) => fav.idDrink === recipe.idDrink)) {
+      set((state) => ({
+        favorites: state.favorites.filter((fav) => fav.idDrink !== recipe.idDrink),
+      }));
+    } else {
+      set((state) => ({
+        favorites: [...state.favorites, recipe],
+        // favorites: [ ...get().favorites, recipe]
+      }));
+    }
+  },
 });
